ef6e4b298d9441c3c8c65dfdffd248c7852e309b
manojkumar135 <manojkumar.chandanada@nowitservices.com>
2025-11-30 00:49:07 +0530

    30 November 2025 2

--- components/PDF/PdfPreview.tsx
+++ components/PDF/PdfPreview.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import PdfViewerClient from "@/components/PDF/PdfViewerClient";
+interface Props {
+  url: string;
+  scale?: number;
+}
 
-export default function PdfPreview({ url, scale }: { url: string; scale?: number }) {
-    return (
-        <div className="bg-black w-[85%] h-[85%] rounded-lg overflow-hidden">
-            <div className="p-2 w-full h-full bg-black">
-                <PdfViewerClient url={url} scale={scale} />
-            </div>
-        </div>
-    );
+export default function PdfPreview({ url }: Props) {
+  return (
+    <div className="bg-black w-[85%] h-[85%] rounded-lg overflow-hidden">
+      <embed
+        src={url}
+        type="application/pdf"
+        className="w-full h-full"
+      />
+    </div>
+  );
 }
