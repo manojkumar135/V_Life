b0325b22fabc7049fddb6f4d75b201bd083bf53c
manojkumar135 <manojkumar.chandanada@nowitservices.com>
2025-11-29 22:50:09 +0530

    29 November 2025 4

--- components/PDF/PdfPreview.tsx
+++ components/PDF/PdfPreview.tsx
@@ -1,28 +1,31 @@
 "use client";
 
-import { Worker, Viewer } from "@react-pdf-viewer/core";
-import "@react-pdf-viewer/core/lib/styles/index.css";
+import { useEffect, useState } from "react";
 
-export default function PdfPreview({
-  url,
-  scale = 0.9,
-}: {
+export default function PdfPreview({ url, scale = 0.9 }: {
   url: string;
   scale?: number;
 }) {
-  if (!url) return null;
+  const [ViewerComp, setViewerComp] = useState<any>(null);
+
+  useEffect(() => {
+    (async () => {
+      const { Worker, Viewer } = await import("@react-pdf-viewer/core");
+      await import("@react-pdf-viewer/core/lib/styles/index.css");
+
+      setViewerComp(() => ({ Worker, Viewer }));
+    })();
+  }, []);
+
+  if (!ViewerComp || !url) return null;
+
+  const { Worker, Viewer } = ViewerComp;
 
   return (
-    <div className="bg-black w-[85%] h-[85%] max-md:w-[90%] max-md:h-[90%] rounded-lg overflow-hidden">
-            <div className="p-2 max-lg:p-0 w-full h-full bg-black rounded-lg">
-    <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js">
-      <Viewer
-        fileUrl={url}
-        defaultScale={scale}
-        theme={{ theme: "dark" }}
-      />
-    </Worker>
-    </div>
+    <div className="bg-black w-[85%] h-[85%] rounded-lg">
+      <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js">
+        <Viewer fileUrl={url} defaultScale={scale} />
+      </Worker>
     </div>
   );
 }
